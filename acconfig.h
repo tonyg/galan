3ce7736025c6b33de3ec649404ed9738071b5996
surazal <surazal>
2001-02-23 08:11:39 +0000

    Initial revision

---
+++ acconfig.h
@@ -0,0 +1,8 @@
+/* Set to 1 if you want to use floating-point internally for
+   all sample representation */
+#define WANT_FLOATING_POINT_SAMPLES 0
+
+@BOTTOM@
+/* Set to the maximum number of samples to process at once
+   in between event-triggers. */
+#define MAXIMUM_REALTIME_STEP 1024

4f3b0f22d38d4351e977ab2c07904d91a5d412f7
torbenh <torbenh@5dd3099f-7335-0410-90d5-f54913b30687>
2006-12-21 02:33:21 +0000

    the missing files of the beta8 commit
    git-svn-id: https://galan.svn.sourceforge.net/svnroot/galan/trunk/galan@249 5dd3099f-7335-0410-90d5-f54913b30687

---
+++ include/galan_jack.h
@@ -0,0 +1,29 @@
+/* gAlan - Graphical Audio Language
+ * Copyright (C) 1999 Tony Garnock-Jones
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <jack/jack.h>
+
+#ifndef GALAN_JACK_H
+#define GALAN_JACK_H
+
+extern jack_client_t *galan_jack_get_client(void);
+
+extern void init_jack( void );
+extern void done_jack( void );
+
+#endif
---
+++ include/galan_lash.h
@@ -0,0 +1,29 @@
+/* gAlan - Graphical Audio Language
+ * Copyright (C) 1999 Tony Garnock-Jones
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <lash/lash.h>
+
+#ifndef GALAN_LASH_H
+#define GALAN_LASH_H
+
+extern lash_client_t *galan_lash_get_client(void);
+
+extern void init_lash( int argc, char **argv );
+extern void done_lash( void );
+
+#endif
